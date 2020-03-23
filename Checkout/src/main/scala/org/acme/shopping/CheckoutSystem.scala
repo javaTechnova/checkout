0b517b9ff306b20c31352aaf07fcc465e35e6f4f
Jayant Bardia <jay.bardia@gmail>
2020-03-23 22:30:43 +0000

    Step 2: Simple offers

--- Checkout/src/main/scala/org/acme/shopping/CheckoutSystem.scala
+++ Checkout/src/main/scala/org/acme/shopping/CheckoutSystem.scala
@@ -3,13 +3,30 @@ package org.acme.shopping
 import org.acme.shopping.Item._
 
 object CheckoutSystem {
+
+  val items = Map(
+    Apple -> ItemPriced(Apple, 0.6),
+    Orange -> ItemPriced(Orange, 0.25))
+
+  def getBasketTotal(basket: Seq[Item],offers: Map[Item,Offer[ItemPriced]] = Map.empty): Double = {
+   if(offers.isEmpty)
+     calculateTotal(deriveItem(basket))
+   else{
+      val grouped = deriveItem(basket).groupBy(f => f)
+      val discountedBasket = grouped.flatMap(i => {
+        offers.get(i._1.name) match {
+          case Some(offer) => offer.discountedItems(i._2)
+          case None => i._2
+        }})
+        calculateTotal(discountedBasket.toSeq)
+      }
+  }
   
-  val items = Map(Apple -> ItemPriced(Apple,0.6),
-                  Orange -> ItemPriced(Orange,0.25))
+  def deriveItem(basket: Seq[Item]) :Seq[ItemPriced] = {
+    basket.map(items.get(_).get)
+  }
   
-  def getBasketTotal(basket:Seq[Item]): Double = {
-    basket.foldLeft(0.0)((total,item) => {
-      total+items.get(item).get.price
-    })
+  def calculateTotal(basket: Seq[ItemPriced]) :Double = {
+    basket.foldLeft(0.0)((total,item) => total+item.price)
   }
 }
--- Checkout/src/test/scala/org/acme/shopping/CheckoutSystemTest.scala
+++ Checkout/src/test/scala/org/acme/shopping/CheckoutSystemTest.scala
@@ -5,6 +5,10 @@ import org.acme.shopping.Item._
 
 class CheckoutSystemTest extends FlatSpec with Matchers {
   
+  val offers = Map(
+    Apple -> new DiscountedOffer("Buy 1 get 1 free", 2, 1),
+    Orange -> new DiscountedOffer("3 for the price of 2", 3, 2))
+  
   it should "handle empty shopping basket" in {
     CheckoutSystem.getBasketTotal(Seq.empty) should be(0.0)
   }
@@ -17,4 +21,25 @@ class CheckoutSystemTest extends FlatSpec with Matchers {
     CheckoutSystem.getBasketTotal(Seq(Apple,Orange,Apple)) should be(1.45)
   }
   
+  it should "apply buy One and get One free offer correctly" in {
+    val offers = Map(Apple -> new DiscountedOffer("Buy 1 get 1 free", 2, 1))
+    CheckoutSystem.getBasketTotal(Seq(Apple,Orange,Apple),offers) should be(0.85)
+  }
+  
+  it should "apply buy Three get two offer correctly" in {
+    val offers = Map(Orange -> new DiscountedOffer("3 for the price of 2", 3, 2))
+    CheckoutSystem.getBasketTotal(Seq(Apple,Orange,Orange,Apple,Orange),offers) should be(1.70)
+  }
+  
+  it should "apply multiple discounts on different items correctly" in {
+    CheckoutSystem.getBasketTotal(Seq(Apple,Orange,Apple,Orange,Orange,Apple,Apple,Apple),offers) should be(2.30)
+  }
+  
+  it should "calculate basket total correctly even when discounts are not applicable" in {
+    CheckoutSystem.getBasketTotal(Seq(Apple,Orange,Orange),offers) should be(1.10)
+  }
+  
+  it should "handle offers correctly on empty basket" in {
+    CheckoutSystem.getBasketTotal(Seq.empty,offers) should be(0.0)
+  }
 }
